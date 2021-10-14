7a13da81708dcf70a6914b1791e20a0693433709
JJ <jos.joha@hotmail.com>
2021-10-14 13:51:30 +0200

    Footer.js fix
    *NEW*
    - Fixed css in the footer component that caused wrong alignment of objects
    - Changed text of objects in footer for more appropriate descriptions

--- website/src/components/Footer.js
+++ website/src/components/Footer.js
@@ -26,9 +26,9 @@ function Footer() {
           </form>
         </div>
       </section>
-      <div className="footer-links">
+      <div class="footer-links">
         <div className="footer-link-wrapper">
-          <div className="footer-link-items">
+          <div class="footer-link-items">
             <h2>About Us</h2>
             <Link to="/sign-up">How it works</Link>
             <Link to="/">Testimonials</Link>
@@ -36,29 +36,28 @@ function Footer() {
             <Link to="/">Investors</Link>
             <Link to="/">Terms of Service</Link>
           </div>
-          <div className="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
+          <div class="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
           </div>
-          <div className="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
+        </div>
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
+            <h2>Videos</h2>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
           </div>
-          <div className="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
+          <div class="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
           </div>
         </div>
       </div>
@@ -71,19 +70,45 @@ function Footer() {
           </div>
           <small className="website-rights">TRVL ©</small>
           <div className="social-icons">
-            <Link className="social-icon-link facebook"
-            to="/"
-            target="_blank"
-            aria-label="Facebook"
+            <Link
+              className="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
             >
               <i className="fab fa-facebook-f"></i>
             </Link>
-            <Link className="social-icon-link instagram"
-            to="/"
-            target="_blank"
-            aria-label="Instagram"
+            <Link
+              className="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
             >
-              <i className="fab fa-instagram-f"></i>
+              <i className="fab fa-instagram"></i>
+            </Link>
+            <Link
+              className="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i className="fab fa-youtube"></i>
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i className="fab fa-twitter"></i>
+            </Link>
+            <Link
+              className="social-icon-link linkedin"
+              to="/"
+              target="_blank"
+              aria-label="Linkedin"
+            >
+              <i className="fab fa-Linkedin"></i>
             </Link>
           </div>
         </div>
